ba538205750d3da09cc1ae6fc4849b5d154c4232
Rahul Gupta <notsonoobiee@gmail.com>
2021-09-29 03:25:15 +0530

    Minor Bug Fixes for Authorizer.

--- microservice/authorizers/jwtauthorizer.js
+++ microservice/authorizers/jwtauthorizer.js
@@ -13,9 +13,11 @@ module.exports.authorizer = async (event, context) => {
         }),
       )
     }
-    return context.fail('Unauthorized')
+    // return context.fail('Unauthorized') // Doesn't work on production - @see https://medium.com/asked-io/serverless-custom-authorizer-issues-on-aws-57a40176f63f
+    return context.succeed(buildIAMPolicy('user', 'Deny', event.methodArn))
   } catch (error) {
     console.log(error, '>>>')
-    return context.fail('Unauthorized')
+    // return context.fail('Unauthorized')
+    return context.succeed(buildIAMPolicy('user', 'Deny', event.methodArn))
   }
 }
