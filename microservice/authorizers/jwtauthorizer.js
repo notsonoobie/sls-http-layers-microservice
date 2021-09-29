939e186c75ab841b2a0c568ad917f5625daec436
Rahul Gupta <notsonoobiee@gmail.com>
2021-09-29 12:47:20 +0530

    Basic Bug fixes for Authorizer.

--- microservice/authorizers/jwtauthorizer.js
+++ microservice/authorizers/jwtauthorizer.js
@@ -3,21 +3,23 @@ const { buildIAMPolicy } = require('../common/utils/awsutils')
 
 module.exports.authorizer = async (event, context) => {
   try {
-    console.log('EVENT FROM AUTHORIZER', event, context)
+    console.error('EVENT FROM AUTHORIZER', event)
     const [bearer, token] = event.authorizationToken?.split(' ')
     if (bearer === 'Bearer' && token) {
       const user = verifyToken(token)
-      return context.succeed(
+      context.succeed(
         buildIAMPolicy(user._id, 'Allow', event.methodArn, {
           user: JSON.stringify(user),
         }),
       )
+      return
     }
     // return context.fail('Unauthorized') // Doesn't work on production - @see https://medium.com/asked-io/serverless-custom-authorizer-issues-on-aws-57a40176f63f
-    return context.succeed(buildIAMPolicy('user', 'Deny', event.methodArn))
+    context.succeed(buildIAMPolicy('user', 'Deny', event.methodArn))
+    return
   } catch (error) {
     console.log(error, '>>>')
-    // return context.fail('Unauthorized')
-    return context.succeed(buildIAMPolicy('user', 'Deny', event.methodArn))
+    context.succeed(buildIAMPolicy('user', 'Deny', event.methodArn))
+    return
   }
 }
