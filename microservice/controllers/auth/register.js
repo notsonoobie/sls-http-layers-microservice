28fdc0d09ca50b114d88f69160b3a3367b77d265
Rahul Gupta <notsonoobiee@gmail.com>
2021-09-30 15:26:58 +0530

    Updated API Gateway Logic.

--- microservice/controllers/auth/register.js
+++ microservice/controllers/auth/register.js
@@ -4,7 +4,11 @@ const { UserModel } = require('../../models/User')
 const { logger } = require('logging')
 const { coreModules, AWS } = require('core')
 const { connectToDb } = require('../../configs/db/connectToDb')
-const { _201Message, _400Message } = require('../../common/messages/messages')
+const {
+  _201Message,
+  _400Message,
+  _200Message,
+} = require('../../common/messages/messages')
 const { generateToken } = require('../../common/auth/jwthelpers')
 const { hashPassword } = require('../../common/auth/hashinghelpers')
 
@@ -16,16 +20,14 @@ module.exports.handler = async (event, context) => {
   try {
     // logger('>>>>>>>>>>>>>>>>>>>>>>>>>>>> CALLING LOGGER FROM REGISTER')
     coreModules.init({
-      accessKeyId: '',
-      secretAccessKey: '',
       region: 'ap-south-1',
     })
-    await coreModules.__createRestApi(
-      'arn:aws:XXXXXXXXXXXX:function:sls-http-layers-microservice-auth-dev-register',
-      'GET',
-      'boommm',
-      'HTTP',
-    )
+    await coreModules.__createRestApi({
+      restApiId: 'nprjg3tyfl',
+      parentPathPart: 'register',
+      newPath: 'debug',
+    })
+    return _200Message()
     await connectToDb()
     const hashedPassword = await hashPassword(req.password)
     req.password = hashedPassword
